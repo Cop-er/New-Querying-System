774726e4aa24f9d5d7505008c16f464bcbe90fc9
floiwebsite <renren.gerald@gmail.com>
2024-09-25 15:46:07 +0800

    Done Birth Query

--- Local Civil Registry System/Birth_Query.cs
+++ Local Civil Registry System/Birth_Query.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,23 +28,73 @@ namespace Local_Civil_Registry_System
 
         private void btn_mother_name_Click(object sender, EventArgs e)
         {
-
+            CheckEntry("Mother");
         }
 
         private void btn_father_name_Click(object sender, EventArgs e)
         {
-
+            CheckEntry("Father");
         }
 
         private void btn_registry_Click(object sender, EventArgs e)
         {
-
+            CheckEntry("Registry");
         }
 
-        private async void btn_child_name_Click(object sender, EventArgs e)
+        private void btn_child_name_Click(object sender, EventArgs e)
+        {
+            CheckEntry("Child");
+        }
+
+        private async void CheckEntry(string _type)
         {
             MongodbConnect mc = new MongodbConnect();
-            await mc.QueryBirthChild("John", "Sm");
+            if (_type == "Child" && !string.IsNullOrWhiteSpace(txtChild_1.Text) && !string.IsNullOrWhiteSpace(txtChild_2.Text))
+            {
+                btn_child_name.Enabled = false;
+                var _results = await mc.QueryBirthChild(txtChild_1.Text, txtChild_2.Text);
+                TableDesign(_results);
+                btn_child_name.Enabled = true;
+            }
+
+            else if (_type == "Mother" && !string.IsNullOrWhiteSpace(txtMother_1.Text) && !string.IsNullOrWhiteSpace(txtMother_2.Text))
+            {
+                btn_mother_name.Enabled = false;
+                var _results = await mc.QueryMotherChild(txtMother_1.Text, txtMother_2.Text);
+                TableDesign(_results);
+                btn_mother_name.Enabled = true;
+            }
+
+            else if (_type == "Father" && !string.IsNullOrWhiteSpace(txtFather_1.Text) && !string.IsNullOrWhiteSpace(txtFather_2.Text))
+            {
+                btn_father_name.Enabled = false;
+                var _results = await mc.QueryFatherChild(txtFather_1.Text, txtFather_2.Text);
+                TableDesign(_results);
+                btn_father_name.Enabled = true;
+            }
+
+            else if (_type == "Registry" && !string.IsNullOrWhiteSpace(txtRegistry.Text))
+            {
+                btn_registry.Enabled = false;
+                var _results = await mc.QueryRegistryNumber(txtRegistry.Text);
+                TableDesign(_results);
+                btn_registry.Enabled = true;
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter both the first name and last name.", "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void TableDesign(DataTable _results)
+        {
+            dataGridView1.DataSource = _results;
+            dataGridView1.Columns["No."].Width = 30;
+            dataGridView1.Columns["Registry Number"].Width = 80;
+            dataGridView1.Columns["MI"].Width = 40;
+            dataGridView1.Columns["Mother MI"].Width = 40;
+            dataGridView1.Columns["Father MI"].Width = 40;
         }
 
 
@@ -51,5 +102,7 @@ namespace Local_Civil_Registry_System
         {
             Application.Exit();
         }
+
+        
     }
 }
--- Local Civil Registry System/MongodbConnect.cs
+++ Local Civil Registry System/MongodbConnect.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using MongoDB.Bson;
@@ -29,9 +30,10 @@ namespace Local_Civil_Registry_System
             }
         }
 
-        public async Task QueryBirthChild(string dt1, string dt2)
+        public async Task<DataTable> QueryBirthChild(string dt1, string dt2)
         {
             ConnectToMongoDB("Birth");
+            List<BsonDocument> _allResults = new List<BsonDocument>();
 
             for (int year = 1920; year <= DateTime.Now.Year; year++)
             {
@@ -40,36 +42,16 @@ namespace Local_Civil_Registry_System
                 var _filterLast = Builders<BsonDocument>.Filter.Regex("LAST", new BsonRegularExpression(dt2, "i"));
                 var _combinedFilter = Builders<BsonDocument>.Filter.And(_filterFirst, _filterLast);
                 var _result = await _collection.Find(_combinedFilter).ToListAsync();
-                foreach (var x in _result)
-                {
-                    Console.WriteLine(x.ToString());
-                }
+                _allResults.AddRange(_result);
             }
-
+            DataTable dataTable = ConvertToDataTable(_allResults);
+            return dataTable;
         }
 
-        public async Task QueryMotherChild(string dt1, string dt2)
+        public async Task<DataTable> QueryFatherChild(string dt1, string dt2)
         {
             ConnectToMongoDB("Birth");
-
-            for (int year = 1920; year <= DateTime.Now.Year; year++)
-            {
-                var _collection = Database.GetCollection<BsonDocument>(year.ToString());
-                var _filterFirst = Builders<BsonDocument>.Filter.Regex("MFIRST", new BsonRegularExpression(dt1, "i"));
-                var _filterLast = Builders<BsonDocument>.Filter.Regex("MLAST", new BsonRegularExpression(dt2, "i"));
-                var _combinedFilter = Builders<BsonDocument>.Filter.And(_filterFirst, _filterLast);
-                var _result = await _collection.Find(_combinedFilter).ToListAsync();
-                foreach (var x in _result)
-                {
-                    Console.WriteLine(x.ToString());
-                }
-            }
-
-        }
-
-        public async Task QueryFatherChild(string dt1, string dt2)
-        {
-            ConnectToMongoDB("Birth");
+            List<BsonDocument> _allResults = new List<BsonDocument>();
 
             for (int year = 1920; year <= DateTime.Now.Year; year++)
             {
@@ -78,15 +60,101 @@ namespace Local_Civil_Registry_System
                 var _filterLast = Builders<BsonDocument>.Filter.Regex("FLAST", new BsonRegularExpression(dt2, "i"));
                 var _combinedFilter = Builders<BsonDocument>.Filter.And(_filterFirst, _filterLast);
                 var _result = await _collection.Find(_combinedFilter).ToListAsync();
-                foreach (var x in _result)
-                {
-                    Console.WriteLine(x.ToString());
-                }
+                _allResults.AddRange(_result);
             }
-
+            DataTable dataTable = ConvertToDataTable(_allResults);
+            return dataTable;
         }
 
+        public async Task<DataTable> QueryMotherChild(string dt1, string dt2)
+        {
+            ConnectToMongoDB("Birth");
+            List<BsonDocument> _allResults = new List<BsonDocument>();
 
+            for (int year = 1920; year <= DateTime.Now.Year; year++)
+            {
+                var _collection = Database.GetCollection<BsonDocument>(year.ToString());
+                var _filterFirst = Builders<BsonDocument>.Filter.Regex("MFIRST", new BsonRegularExpression(dt1, "i"));
+                var _filterLast = Builders<BsonDocument>.Filter.Regex("MLAST", new BsonRegularExpression(dt2, "i"));
+                var _combinedFilter = Builders<BsonDocument>.Filter.And(_filterFirst, _filterLast);
+                var _result = await _collection.Find(_combinedFilter).ToListAsync();
+                _allResults.AddRange(_result);
+            }
+            DataTable dataTable = ConvertToDataTable(_allResults);
+            return dataTable;
+        }
+
+        public async Task<DataTable> QueryRegistryNumber(string dt1)
+        {
+            ConnectToMongoDB("Birth");
+            List<BsonDocument> _allResults = new List<BsonDocument>();
+
+            for (int year = 1920; year <= DateTime.Now.Year; year++)
+            {
+                var _collection = Database.GetCollection<BsonDocument>(year.ToString());
+                var _filterFirst = Builders<BsonDocument>.Filter.Regex("LCR", new BsonRegularExpression(dt1, "i"));
+                var _result = await _collection.Find(_filterFirst).ToListAsync();
+                _allResults.AddRange(_result);
+            }
+            DataTable dataTable = ConvertToDataTable(_allResults);
+            return dataTable;
+        }
+
+        private DataTable ConvertToDataTable(List<BsonDocument> documents)
+        {
+            var columnsToInclude = new Dictionary<string, string>
+            {
+                { "CT", "No." },
+                { "LCR", "Registry Number" },
+                { "FIRST", "First Name" },
+                { "MI", "MI" },
+                { "LAST", "Last Name" },
+                { "MFIRST", "Mother First Name" },
+                { "MMI", "Mother MI" },
+                { "MLAST", "Mother Last Name" },
+                { "FFIRST", "Father First Name" },
+                { "FMI", "Father MI" },
+                { "FLAST", "Father Last Name" },
+                { "DATE", "Date of Birth" },
+                { "SEX", "Sex (Male: 1, Female: 2)" },
+                { "FOL", "Book Number" },
+                { "PAGE", "Page Number" },
+                { "DATEMAR", "Date of Marriage" },
+                { "DREG", "Date of Registration" },
+
+
+            };
+
+            DataTable dataTable = new DataTable();
+
+            foreach (var field in columnsToInclude)
+            {
+                dataTable.Columns.Add(field.Value, typeof(string));
+            }
+
+            int i = 1;
+
+            foreach (var document in documents)
+            {
+                var row = dataTable.NewRow();
+                foreach (var column in columnsToInclude.Keys)
+                {
+                    if (document.Contains(column))
+                    {
+                        if (column == "CT") // Use the key directly for comparison
+                        {
+                            row[columnsToInclude[column]] = i++; // Assign the incrementing number
+                         } else
+                        {
+                           row[columnsToInclude[column]] = document[column].ToString();
+                        }
+                    }
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
 
     }
 }
